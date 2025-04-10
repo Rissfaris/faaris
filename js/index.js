7bc7f514a701e812a4fb966c2f1a794d37f70e54
faaris <doanghosting@gmail.com>
2025-04-10 12:59:54 +0700

    index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'halooo ka tasyaa'.split('')
+const text = 'kak tasyaa'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
